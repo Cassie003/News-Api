c2ee2f92af598d09a673daaade5ec31935862d54
Cassie003 <caseyminu@gmail.com>
2022-05-03 15:46:29 +0300

    test articles

---
+++ tests/test_article.py
@@ -0,0 +1,20 @@
+import unittest
+from app.models import News
+
+class NewsTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Movie class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_news = News('BBC SPORT',"Sala hits 100th Goal in Liverpool win",'https://www.bbc.com/sport/football/58437041','Sports')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+        
+        
+#if __name__ == '__main__':
+#     unittest.main()        
